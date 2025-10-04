a17c18848fe5639077ec2c63e022d8b65e6d827c
Nadezhda Zhuzhleva <nzhuzhle@car3s1.42barcelona.com>
2025-10-04 16:03:04 +0200

    only potential threat

--- data/neo.py
+++ data/neo.py
@@ -1,26 +1,64 @@
 import requests
 import pprint
 import json
+import math
+import pandas as pd
+from datetime import datetime
 
 NASA_API_KEY = "9q1BIrXXNJuWvxQLWkedidQgYXlPNOJFtodj6gTl"
+EARTH_RADIUS_KM = 6371.0
 
-url = f"https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={NASA_API_KEY}"
+url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
 
-response = requests.get(url)
-data = response.json()
+params = {"api_key": NASA_API_KEY}
+hazardous_asteroids = []
+
+# url = f"https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={NASA_API_KEY}"
+
+# response = requests.get(url)
+# data = response.json()
+
+for page in range(0, 3):  # you can increase pages if needed
+    params["page"] = page
+    res = requests.get(url, params=params)
+    if res.status_code != 200:
+        print("Error:", res.status_code)
+        break
+
+    data = res.json()
+    neos = data["near_earth_objects"]
+    print("NEW PAGE")
+    for neo in neos:
+        if neo["is_potentially_hazardous_asteroid"]:
+            name = neo["name"]
+            diameter = neo["estimated_diameter"]["meters"]["estimated_diameter_max"]
+            nasa_jpl_url = neo["nasa_jpl_url"]
+            hazardous_asteroids.append({
+                "name": name,
+                "diameter_m": diameter,
+                "url": nasa_jpl_url
+            })
+
+    # Stop if there's no next page
+    if not data["page"]["number"] < data["page"]["total_pages"] - 1:
+        break
+
+print(f"Total hazardous asteroids found: {len(hazardous_asteroids)}")
+for asteroid in hazardous_asteroids[:10]:
+    print(f"{asteroid['name']} — {asteroid['diameter_m']:.1f} m")
 
 # Print first asteroid summary
-asteroids = data.get("near_earth_objects", [])
-print(f"Retrieved {len(asteroids)} asteroids")
+# asteroids = data.get("near_earth_objects", [])
+print(f"Retrieved {len(hazardous_asteroids)} asteroids")
 
-if asteroids:
-    first = asteroids[0]
-    print("Name:", first["name"])
-    print("Diameter (km):", first["estimated_diameter"]["kilometers"]["estimated_diameter_max"])
-    print("Velocity (km/s):", first["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"])
-    pprint.pprint(first["close_approach_data"][0])
+# if asteroids:
+#     first = asteroids[0]
+#     print("Name:", first["name"])
+#     print("Diameter (km):", first["estimated_diameter"]["kilometers"]["estimated_diameter_max"])
+#     print("Velocity (km/s):", first["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"])
+#     pprint.pprint(first["close_approach_data"][0])
 
-
+print("\n#########################################################\n")
 #####################################################################
 
 # Base URL for the USGS Earthquake API
